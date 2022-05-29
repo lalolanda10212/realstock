7c567b99fe1115557626aeb211c91ac2d6bb035d
Sergio <checho140140@gmail.com>
2022-05-29 17:14:13 -0500

    modales de registro y actualizar usuarios

---
+++ script.js
@@ -0,0 +1,24 @@
+const btnModals = document.querySelectorAll('[data-btn-modal="true"]')
+btnModals.forEach(element => {
+    element.addEventListener("click", (e) => {
+        e.preventDefault;
+        let idModal = element.getAttribute("data-modal");
+        const modalWrapper = document.querySelector(idModal);
+        const modal = document.querySelector(`${idModal} .modal`);
+        modalWrapper.classList.add("visible");
+        modal.classList.add("visible");
+    });
+});
+
+const btnCloseModal = document.querySelectorAll('[data-btn-close="modal"]');
+btnCloseModal.forEach(element => {
+    const modalWrapper = element.parentNode.parentNode.parentNode;
+    modalWrapper.addEventListener("click", (e) => {
+        e.preventDefault;
+        const modal = element.parentNode.parentNode;
+        if (e.target == element || e.target == modalWrapper) {
+            modalWrapper.classList.remove("visible");
+            modal.classList.remove("visible");
+        }
+    });
+});
